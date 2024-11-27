56dcac39c43904749a1f5bd75c3e92ba4d8beec2
Devin Aebi <45836403+aebid@users.noreply.github.com>
2024-11-27 13:19:18 +0100

    mu_p not needed for lowPt correction, was left from copying other code
    Co-authored-by: Konstantin Androsov <kandrosov@users.noreply.github.com>

--- mu.h
+++ mu.h
@@ -359,7 +359,6 @@ public:
     float getLowPtMuonSF(const LorentzVectorM & muon_p4, const float Muon_pfRelIso04_all, const bool Muon_TightId, const float Muon_tkRelIso, const bool Muon_highPtId, UncSource source, UncScale scale) const {
         const UncScale muID_scale = sourceApplies(source, Muon_pfRelIso04_all, Muon_TightId, muon_p4.Pt(), Muon_tkRelIso, Muon_highPtId) ? scale : UncScale::Central;
         const std::string& scale_str = getScaleStr(muID_scale);
-        const auto mu_p = std::hypot(muon_p4.Px(),muon_p4.Py(),muon_p4.Pz());
         return source == UncSource::Central ? 1. : lowPtmuCorrections.at(getUncSourceName(source))->evaluate({abs(muon_p4.Eta()),muon_p4.Pt(), scale_str}) ;
     }
 
