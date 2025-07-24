5d0e2cf5dc48c52900d9ce2341b64720be26cfd0
Valeria D'Amante <valeriadamante@gmail.com>
2025-07-24 08:32:00 +0200

    fixed naming for JWM

--- JetVetoMap.h
+++ JetVetoMap.h
@@ -11,11 +11,11 @@ namespace correction {
         corrections_(CorrectionSet::from_file(fileName))
         {
             std::cout << "JetVetoMapProvider: init" << std::endl;
-            JetVetoMap_value = corrections_->at(entry_name); //bisogna capire il nome delle ere
-
+            JetVetoMap_value = corrections_->at(entry_name);
         }
         RVecB GetJetVetoMapValues(const RVecLV& Jet_p4) const {
-            RVecB veto_values(Jet_p4.size(), 1.0); // Default value is 1.0 (no veto)
+            // Non-zero value for (eta, phi) indicates that the region is vetoed. --> initialization with 0.
+            RVecB veto_values(Jet_p4.size(), 0.); // Default value is 0.0 (NO veto)
             for (int jet_idx = 0; jet_idx < Jet_p4.size(); jet_idx++) {
                 float jvm_factor = JetVetoMap_value->evaluate({"jetvetomap", Jet_p4[jet_idx].Eta(),Jet_p4[jet_idx].Phi()}) ;
                 veto_values[jet_idx] = static_cast<bool>(jvm_factor); // Apply the correction factor
