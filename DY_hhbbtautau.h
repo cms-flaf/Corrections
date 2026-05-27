3640dffc703eb1bc3513c7aefb47cc9e5451c7d3
Sayan Dhani <sayandhani1234@gmail.com>
2026-05-27 14:49:21 +0200

    Dy corr hh bbtautau (#108)
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    * DY Correction for HH_bbtautau
    
    ---------
    
    Co-authored-by: Sayan <sadhani@cern.ch>

---
+++ DY_hhbbtautau.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+
+#include "correction.h"
+#include "corrections.h"
+
+namespace correction {
+
+    class DYbbtautauCorrProvider : public CorrectionsBase<DYbbtautauCorrProvider> {
+      public:
+        explicit DYbbtautauCorrProvider(const std::string& fileName)
+            : corrections_(CorrectionSet::from_file(fileName)), dyWeight_(corrections_->at("dy_weight")) {}
+
+        template <typename LV1, typename LV2>
+        double getWeight(const std::string& era,
+                         int njets,
+                         int ntags,
+                         const LV1& tau1_gen_p4,
+                         const LV2& tau2_gen_p4,
+                         const std::string& syst,
+                         bool isValid) const {
+            const float ptll = static_cast<float>((tau1_gen_p4 + tau2_gen_p4).Pt());
+            static constexpr int kMinNJets = 2;
+
+            double weight = 1.0;
+
+            if (!isValid) {
+                weight = 1.0;
+            } else {
+                if (njets < kMinNJets) {
+                    weight = 1.0;
+                } else {
+                    weight = safeEvaluate(dyWeight_, era, njets, ntags, ptll, syst);
+                }
+            }
+            return weight;
+        }
+
+      private:
+        std::unique_ptr<CorrectionSet> corrections_;
+        Correction::Ref dyWeight_;
+    };
+
+}  // namespace correction
