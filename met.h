f3dfd20748660e82a93f9c8eef54e319b7dd02b9
Devin <aebid@tamu.edu>
2026-06-08 22:00:42 +0200

    Rework MET to be computed on deltas, not on nano-shift

--- met.h
+++ met.h
@@ -5,30 +5,25 @@
 namespace correction {
 
     LorentzVectorM ShiftMet(const LorentzVectorM& met_p4,
-                            const std::vector<RVecLV>& p4_original,
-                            const std::vector<RVecLV>& p4_shifted,
+                            const std::vector<RVecLV>& p4_delta,
                             bool debug) {
         if (debug) {
             std::string lvstring_met_original = LorentzVectorToString(met_p4, analysis::LVectorRepr::PxPyPtPhi);
             std::cout << "MET original = " << lvstring_met_original << std::endl;
         }
         LorentzVectorXYZ met_p4_shifted(met_p4);
-        for (size_t collection_idx = 0; collection_idx < p4_original.size(); ++collection_idx) {
-            for (size_t obj_idx = 0; obj_idx < p4_original.at(collection_idx).size(); ++obj_idx) {
-                const auto delta_p4 =
-                    p4_shifted.at(collection_idx).at(obj_idx) - p4_original.at(collection_idx).at(obj_idx);
+        for (size_t collection_idx = 0; collection_idx < p4_delta.size(); ++collection_idx) {
+            for (size_t obj_idx = 0; obj_idx < p4_delta.at(collection_idx).size(); ++obj_idx) {
+                const auto delta_p4 = p4_delta.at(collection_idx).at(obj_idx);
                 met_p4_shifted -= delta_p4;
                 if (debug) {
-                    std::string lvstring_original = LorentzVectorToString(p4_original.at(collection_idx).at(obj_idx),
+                    std::string lvstring_delta = LorentzVectorToString(p4_delta.at(collection_idx).at(obj_idx),
                                                                           analysis::LVectorRepr::PxPyPtPhi);
-                    std::string lvstring_shifted = LorentzVectorToString(p4_shifted.at(collection_idx).at(obj_idx),
-                                                                         analysis::LVectorRepr::PxPyPtPhi);
                     std::string lvstring_met = LorentzVectorToString(met_p4, analysis::LVectorRepr::PxPyPtPhi);
                     std::string lvstring_met_shifted =
                         LorentzVectorToString(met_p4_shifted, analysis::LVectorRepr::PxPyPtPhi);
                     std::cout << " collection idx = " << collection_idx << " obj index = " << obj_idx
-                              << " p4 original = " << lvstring_original << " p4 shifted = " << lvstring_shifted
-                              << " delta p4 = " << delta_p4 << " met_p4 = " << lvstring_met << " met_p4 shifted "
+                              << " p4 delta = " << lvstring_delta << " met_p4 = " << lvstring_met << " met_p4 shifted "
                               << lvstring_met_shifted << std::endl;
                 }
             }
